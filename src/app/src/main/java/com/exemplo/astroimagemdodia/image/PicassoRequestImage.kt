37272046bfca2592f486b2f5c0560ce4d1ba258c
Danilo Abranches <daniloeabranches@gmail.com>
2020-04-08 21:00:01 -0300

    Exclusão de classes de teste não utilizadas

--- src/app/src/main/java/com/exemplo/astroimagemdodia/image/PicassoRequestImage.kt
+++ src/app/src/main/java/com/exemplo/astroimagemdodia/image/PicassoRequestImage.kt
@@ -1,11 +1,12 @@
 package com.exemplo.astroimagemdodia.image
 
 import android.widget.ImageView
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 
 class PicassoRequestImage : RequestImage {
     override fun load(url: String, view: ImageView, callback: RequestImage.Callback) {
-        Picasso.get().load(url).into(view, object : com.squareup.picasso.Callback {
+        Picasso.get().load(url).into(view, object : Callback {
             override fun onSuccess() = callback.success()
             override fun onError(e: Exception?) = callback.error(e)
         })
